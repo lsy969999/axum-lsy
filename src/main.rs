a27c00e199ed4cd4684199866862668a812beab8
lsy969999 <lsy969999@gmail.com>
2023-12-26 01:59:14 +0900

    databse

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use axum::{routing::get, Router};
+use sqlx::PgPool;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn main(#[shuttle_shared_db::Postgres(
+  local_uri = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
+)] pool: PgPool,) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+          .route("/", get(hello_world))
+          .with_state(pool);
 
     Ok(router.into())
 }
