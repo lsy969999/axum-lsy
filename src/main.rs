575e228ae546bbd62c7568fab7f93cc85ba53505
lsy969999 <lsy969999@gmail.com>
2023-12-26 02:07:25 +0900

    db test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, extract::State, response::{Html, IntoResponse}};
 use sqlx::PgPool;
+use tracing::info;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world(State(pool): State<PgPool>) -> impl IntoResponse {
+  let row: (i64,) = sqlx::query_as("SELECT $1")
+    .bind(150_i64).fetch_one(&pool).await.expect("db fetch fail!");
+    let st = format!("hello world {}", row.0.to_string());
+    Html(st)
 }
 
 #[shuttle_runtime::main]
@@ -12,6 +16,6 @@ async fn main(#[shuttle_shared_db::Postgres(
     let router = Router::new()
           .route("/", get(hello_world))
           .with_state(pool);
-
+    info!("hi");
     Ok(router.into())
 }
