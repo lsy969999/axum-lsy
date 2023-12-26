351afa45f7a0583541d8f9c5ae7f9e0de668a549
seongyun.lim <seongyun.lim@e4net.net>
2023-12-26 10:13:27 +0900

    add secret

---
+++ readme.md
@@ -0,0 +1,3 @@
+```
+cargo shuttle run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use askama::Template;
 use axum::{routing::get, Router, extract::State, response::{Html, IntoResponse, Response}, http::StatusCode};
+use shuttle_secrets::SecretStore;
 use sqlx::PgPool;
 use tower_http::services::ServeDir;
 use tracing::info;
+use anyhow::anyhow;
 
 async fn hello_world(State(pool): State<PgPool>) -> impl IntoResponse {
   let row: (i64,) = sqlx::query_as("SELECT $1")
@@ -12,9 +14,18 @@ async fn hello_world(State(pool): State<PgPool>) -> impl IntoResponse {
 }
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres(
-  local_uri = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
-)] pool: PgPool,) -> shuttle_axum::ShuttleAxum {
+async fn main(
+  #[shuttle_shared_db::Postgres(
+    local_uri = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
+  )] pool: PgPool,
+  #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    let env = if let Some(secret) = secret_store.get("ENV"){
+      secret
+    } else {
+      return Err(anyhow!("secret was not found").into());
+    };
+    info!("hi env is {}", env);
     let router = Router::new()
           .route("/hello_world", get(hello_world))
           .route("/", get(idx))
@@ -22,8 +33,8 @@ async fn main(#[shuttle_shared_db::Postgres(
             Html("<span class='test'>haha</span><script>console.log('dudu');</script>")
           }))
           .nest_service("/assets", ServeDir::new("assets"))
-          .with_state(pool);
-    info!("hi");
+          .with_state(pool)
+          .with_state(secret_store);
     Ok(router.into())
 }
 
